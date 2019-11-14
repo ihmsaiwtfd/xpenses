948d881d2ff683789b82263eb63e0d17c49da400
ihmsaiwtfd <31773241+ihmsaiwtfd@users.noreply.github.com>
2019-11-14 17:56:44 +0500

    missing files for previous commit

---
+++ Core/UseCases/GetEntriesUseCase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Dto;
+using Core.Interfaces;
+using Core.Interfaces.UseCases;
+
+namespace Core.UseCases
+{
+    internal class GetEntriesUseCase : IGetEntriesUseCase
+    {
+        private readonly IRepository<Entry> _Repo;
+
+        public GetEntriesUseCase(IRepository<Entry> repo)
+        {
+            _Repo = repo;
+        }
+
+        public async Task<bool> Handle(GetEntriesRequest message, IOutputPort<GetEntriesResponse> outputPort)
+        {
+            Entry[] entries = null;
+            try
+            {
+                await Task.Run(() =>
+                {
+                    entries = _Repo.List(message.Query).ToArray();
+                });
+                outputPort.Handle(new GetEntriesResponse(entries, true, null));
+                return true;
+            }
+            catch (Exception ex)
+            {
+                outputPort.Handle(new GetEntriesResponse(null, false, ex.ToString()));
+                return false;
+            }
+        }
+    }
+}
---
+++ Core/UseCases/UpdateCategoryRelationsUseCase.cs
@@ -0,0 +1,53 @@
+﻿using Core.Dto;
+using Core.Interfaces;
+using Core.Interfaces.UseCases;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.UseCases
+{
+    internal class UpdateCategoryRelationsUseCase : IUpdateCategoryRelationsUseCase
+    {
+        private readonly IRepository<Category> _CatRepo;
+        private readonly IRepository<CategoryRelation> _RelRepo;
+
+        public UpdateCategoryRelationsUseCase(IRepository<Category> catRepo, IRepository<CategoryRelation> relRepo)
+        {
+            _CatRepo = catRepo;
+            _RelRepo = relRepo;
+        }
+
+        public async Task<bool> Handle(UpdateCategoryRelationsRequest message, IOutputPort<UpdateCategoryRelationsResponse> outputPort)
+        {
+            try
+            {
+                await Task.Run(() =>
+                {
+                    if (_CatRepo.GetById(message.Category) == null)
+                        throw new ArgumentException("No category with such id.");
+
+                    if (_CatRepo.List(o => message.Parents.Contains(o.Uid)).Count() != message.Parents.Length)
+                        throw new ArgumentException("Not all parent categories exist.");
+
+                    CategoryRelation[] relations = _RelRepo.List(o => o.ChildUid == message.Category).ToArray();
+                    foreach (CategoryRelation rel in relations.Where(o => !message.Parents.Contains(o.ParentUid)))
+                    {
+                        _RelRepo.Delete(rel);
+                    }
+                    foreach (Guid newParent in message.Parents.Where(o => !relations.Any(r => r.ParentUid == o)))
+                    {
+                        _RelRepo.Add(new CategoryRelation() { ChildUid = message.Category, ParentUid = newParent });
+                    }
+                });
+                outputPort.Handle(new UpdateCategoryRelationsResponse(true, null));
+                return true;
+            }
+            catch (Exception ex)
+            {
+                outputPort.Handle(new UpdateCategoryRelationsResponse(false, ex.ToString()));
+                return false;
+            }
+        }
+    }
+}
